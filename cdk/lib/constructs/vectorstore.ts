23dee36bd996f1f8515c920a266fa06c0d9fdc0c
Khatrani Kishan <support17@electromech.info>
2024-04-08 17:52:07 +0530

    update logo

--- cdk/lib/constructs/vectorstore.ts
+++ cdk/lib/constructs/vectorstore.ts
@@ -38,7 +38,7 @@ export class VectorStore extends Construct {
       enableDataApi: true,
       storageEncrypted: props.dbEncryption,
       serverlessV2MinCapacity: 0.5,
-      serverlessV2MaxCapacity: 5.0,
+      serverlessV2MaxCapacity: 2.0,
       writer: rds.ClusterInstance.serverlessV2("writer", {
         autoMinorVersionUpgrade: false,
       }),
