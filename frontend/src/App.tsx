c30b6955d186f36d969a45bc351cff8afa1cf48e
Khatrani Kishan <support17@electromech.info>
2024-05-01 10:35:08 +0530

    update code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react';
+import { translations } from '@aws-amplify/ui-react';
+import { Amplify, I18n } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import AuthAmplify from './components/AuthAmplify';
+import AuthCustom from './components/AuthCustom';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { useTranslation } from 'react-i18next';
+import './i18n';
+import { validateSocialProvider } from './utils/SocialProviderUtils';
+import AppContent from './components/AppContent';
+
+const customProviderEnabled =
+  import.meta.env.VITE_APP_CUSTOM_PROVIDER_ENABLED === 'true';
+const socialProviderFromEnv = import.meta.env.VITE_APP_SOCIAL_PROVIDERS?.split(
+  ','
+).filter(validateSocialProvider);
+
+const App: React.FC = () => {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    // set header title
+    document.title = t('app.name');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  Amplify.configure({
+    Auth: {
+      userPoolId: import.meta.env.VITE_APP_USER_POOL_ID,
+      userPoolWebClientId: import.meta.env.VITE_APP_USER_POOL_CLIENT_ID,
+      authenticationFlowType: 'USER_SRP_AUTH',
+      oauth: {
+        domain: import.meta.env.VITE_APP_COGNITO_DOMAIN,
+        scope: ['openid', 'email'],
+        redirectSignIn: import.meta.env.VITE_APP_REDIRECT_SIGNIN_URL,
+        redirectSignOut: import.meta.env.VITE_APP_REDIRECT_SIGNOUT_URL,
+        responseType: 'code',
+      },
+    },
+  });
+
+  I18n.putVocabularies(translations);
+  I18n.setLanguage(i18n.language);
+
+  return (
+    <>
+      {customProviderEnabled ? (
+        <AuthCustom>
+          <AppContent />
+        </AuthCustom>
+      ) : (
+        <Authenticator.Provider>
+          <AuthAmplify socialProviders={socialProviderFromEnv}>
+            <AppContent />
+          </AuthAmplify>
+        </Authenticator.Provider>
+      )}
+    </>
+  );
+};
+
+export default App;
