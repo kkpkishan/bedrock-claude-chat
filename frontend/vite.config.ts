cbaddc00f9189f828e4d05eafefb4bd84f193520
Khatrani Kishan <support17@electromech.info>
2024-05-01 09:58:08 +0530

    remvoe code

--- frontend/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: { alias: { './runtimeConfig': './runtimeConfig.browser' } },
-  plugins: [react()],
-  server: { host: true },
-});
